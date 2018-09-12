d60f3e20a5ce07031b2318aeab37c283e9d67cf4
Dave Weilert <daveweilert@us.ibm.com>
2018-09-12 16:07:34 -0500

    update arch

--- scripts/boot-master/start_install.sh
+++ scripts/boot-master/start_install.sh
@@ -5,5 +5,7 @@ source /tmp/icp-bootmaster-scripts/functions.sh
 # This will populate $org $repo and $tag
 parse_icpversion ${1}
 echo "org=$org repo=$repo tag=$tag"
+echo "Got first parameter $1" 
+tag1=$1
 
-docker run -e LICENSE=accept --net=host -t -v /opt/ibm/cluster:/installer/cluster ${org}/${repo}:${tag} install
+docker run -e LICENSE=accept --net=host -t -v /opt/ibm/cluster:/installer/cluster ${org}/${repo}:${tag1} install
